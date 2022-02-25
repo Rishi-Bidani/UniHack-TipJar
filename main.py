007d47642a703f8e275896d8202cb7719bb7e505
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2022-02-25 22:22:02 +1100

    first commit - started the project with hello world homepage

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+# HOME
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+# LOGIN
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        """
+        Implement login logic
+            - Get username from form
+            - Get password from form
+                -> Login if correct -> redirect to users/username/edit
+                -> Send error response if wrong
+        """
+        pass
+    else:
+        return render_template("404.html")
+
+
+# WHAT THE DONATOR WILL SEE
+@app.route("/user/<username>")
+def user():
+    pass
+
+
+if __name__ == '__main__':
+    # Change to false when in production
+    app.run(debug=True)
